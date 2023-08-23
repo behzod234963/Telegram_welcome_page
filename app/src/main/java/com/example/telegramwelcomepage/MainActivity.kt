9ea40f38073f5bdbf2d0003c9dfe9b20c6e2d969
Behzod96447 <behzod234963@internet.ru>
2023-08-23 16:14:46 +0500

    Tg welcome page

--- app/src/main/java/com/example/telegramwelcomepage/MainActivity.kt
+++ app/src/main/java/com/example/telegramwelcomepage/MainActivity.kt
@@ -1,18 +1,26 @@
 package com.example.telegramwelcomepage
 
 import android.annotation.SuppressLint
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.view.View.INVISIBLE
-import android.view.View.VISIBLE
+import android.view.View.GONE
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
-import android.widget.TextView
-import androidx.core.graphics.scaleMatrix
-import com.airbnb.lottie.LottieAnimationView
+import android.widget.RelativeLayout
+import androidx.fragment.app.Fragment
+import com.example.telegramwelcomepage.fragments.CloudBasedFragment
+import com.example.telegramwelcomepage.fragments.CloudBasedFragmentDark
+import com.example.telegramwelcomepage.fragments.FastFragment
+import com.example.telegramwelcomepage.fragments.FastFragmentDark
+import com.example.telegramwelcomepage.fragments.GiftFragment
+import com.example.telegramwelcomepage.fragments.GiftFragmentDark
+import com.example.telegramwelcomepage.fragments.PowerfulFragment
+import com.example.telegramwelcomepage.fragments.PowerfulFragmentDark
+import com.example.telegramwelcomepage.fragments.SecureFragment
+import com.example.telegramwelcomepage.fragments.SecureFragmentDark
+import com.example.telegramwelcomepage.fragments.TelegramFragment
+import com.example.telegramwelcomepage.fragments.TelegramFragmentDark
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,24 +30,14 @@ class MainActivity : AppCompatActivity() {
         initViews()
 
     }
-
-    @SuppressLint("ResourceAsColor", "SetTextI18n")
+    @SuppressLint("ResourceAsColor")
     private fun initViews() {
         var theme=0
         var switch:Int=1
-
-        val llRoot:LinearLayout=findViewById(R.id.llRoot)
         val ivTheme:ImageView=findViewById(R.id.ivTheme)
-        val lavMainAnim:LottieAnimationView=findViewById(R.id.lavMainAnim)
-        val tvTitle:TextView=findViewById(R.id.tvTitle)
-        val tvText:TextView=findViewById(R.id.tvText)
-        val ivSwitchOne:ImageView=findViewById(R.id.ivSwitch1)
-        val ivSwitchTwo:ImageView=findViewById(R.id.ivSwitch2)
-        val ivSwitchThree:ImageView=findViewById(R.id.ivSwitch3)
-        val ivSwitchFour:ImageView=findViewById(R.id.ivSwitch4)
-        val ivSwitchFive:ImageView=findViewById(R.id.ivSwitch5)
-        val ivSwitchSix:ImageView=findViewById(R.id.ivSwitch6)
         val btnStart:Button=findViewById(R.id.btnStart)
+        val rlDark:RelativeLayout=findViewById(R.id.rlBlack)
+        val rlWhite:RelativeLayout=findViewById(R.id.rlWhite)
 
         btnStart.setOnClickListener {
 
@@ -49,67 +47,99 @@ class MainActivity : AppCompatActivity() {
 
                 1->{
 
-                    lavMainAnim.setAnimation("telegram.json")
-                    tvTitle.text="Telegram"
-                    tvText.text="The world's fastest messaging app.\nIt is free and secure."
-                    ivSwitchSix.setImageResource(R.drawable.pic_circlegrey)
-                    ivSwitchOne.setImageResource(R.drawable.pic_circleblue)
-                    lavMainAnim.playAnimation()
+                    if (theme==1){
+                        rlWhite.visibility=GONE
+                        val telegramFragmentDark=TelegramFragmentDark()
+                        setFragment(telegramFragmentDark)
+                        ivTheme.setImageResource(R.drawable.pic_daytheme)
+
+                    }else{
+
+                        val telegramFragment=TelegramFragment()
+                        setFragment(telegramFragment)
+
+                    }
 
                 }
 
                 2->{
 
-                    lavMainAnim.setAnimation("fast.json")
-                    tvTitle.text="Fast"
-                    tvText.text="Telegram delivers messages\nfaster than any other application"
-                    ivSwitchOne.setImageResource(R.drawable.pic_circlegrey)
-                    ivSwitchTwo.setImageResource(R.drawable.pic_circleblue)
-                    lavMainAnim.playAnimation()
+                    if (theme==1){
+
+                        val fastFragmentDark=FastFragmentDark()
+                        setFragment(fastFragmentDark)
+
+                    }else{
+
+                        val fastFragment = FastFragment()
+                        setFragment(fastFragment)
+
+                    }
 
                 }
 
                 3->{
 
-                    lavMainAnim.setAnimation("gift.json")
-                    tvTitle.text="Free"
-                    tvText.text="Telegram provides free unlimited\ncloud storage for chats and media"
-                    ivSwitchTwo.setImageResource(R.drawable.pic_circlegrey)
-                    ivSwitchThree.setImageResource(R.drawable.pic_circleblue)
-                    lavMainAnim.playAnimation()
+                    if (theme==1){
+
+                        val giftFragmentDark=GiftFragmentDark()
+                        setFragment(giftFragmentDark)
+
+                    }else{
+
+                        val giftFragment=GiftFragment()
+                        setFragment(giftFragment)
+
+                    }
 
                 }
 
                 4->{
 
-                    lavMainAnim.setAnimation("infinite.json")
-                    tvTitle.text="Powerful"
-                    tvText.text="Telegram has no limits on\nthe size of your media and chats"
-                    ivSwitchThree.setImageResource(R.drawable.pic_circlegrey)
-                    ivSwitchFour.setImageResource(R.drawable.pic_circleblue)
-                    lavMainAnim.playAnimation()
+                    if (theme==1){
+
+                        val powerfulFragmentDark=PowerfulFragmentDark()
+                        setFragment(powerfulFragmentDark)
+
+                    }else{
+
+                        val powerfulFragment=PowerfulFragment()
+                        setFragment(powerfulFragment)
+
+                    }
 
                 }
 
                 5->{
 
-                    lavMainAnim.setAnimation("safe.json")
-                    tvTitle.text="Secure"
-                    tvText.text="Telegram keeps your messages safe\nfrom hacker attacks"
-                    ivSwitchFour.setImageResource(R.drawable.pic_circlegrey)
-                    ivSwitchFive.setImageResource(R.drawable.pic_circleblue)
-                    lavMainAnim.playAnimation()
+                    if (theme==1){
+
+                        val secureFragmentDark=SecureFragmentDark()
+                        setFragment(secureFragmentDark)
+
+                    }else{
+
+                        val secureFragment=SecureFragment()
+                        setFragment(secureFragment)
+
+                    }
 
                 }
 
                 6->{
 
-                    lavMainAnim.setAnimation("cloud.json")
-                    tvTitle.text="Cloud-Based"
-                    tvText.text="Telegram lets you access your\nmessages from multiple devices."
-                    ivSwitchFive.setImageResource(R.drawable.pic_circlegrey)
-                    ivSwitchSix.setImageResource(R.drawable.pic_circleblue)
-                    lavMainAnim.playAnimation()
+                    if (theme==1){
+
+                        val cloudBasedFragmentDark=CloudBasedFragmentDark()
+                        setFragment(cloudBasedFragmentDark)
+
+                    }else{
+
+                        val cloudBasedFragment=CloudBasedFragment()
+                        setFragment(cloudBasedFragment)
+
+                    }
+
                     switch=0
 
                 }
@@ -121,26 +151,43 @@ class MainActivity : AppCompatActivity() {
         ivTheme.setOnClickListener {
 
             theme++
-
             when(theme){
 
                 1->{
 
-                    ivTheme.setImageResource(R.drawable.pic_daytheme)
                     llRoot.setBackgroundColor(R.color.black)
-                    tvTitle.setTextColor(R.color.white)
-                    tvText.setTextColor(R.color.white1)
-                    tvTitle.setTextColor(R.color.white1)
-                    tvText.setTextColor(R.color.white1)
+                    val telegramFragmentDark=TelegramFragmentDark()
+                    setFragment(telegramFragmentDark)
+                    val fastFragmentDark=FastFragmentDark()
+                    setFragment(fastFragmentDark)
+                    val giftFragmentDark=GiftFragmentDark()
+                    setFragment(giftFragmentDark)
+                    val powerfulFragmentDark=PowerfulFragmentDark()
+                    setFragment(powerfulFragmentDark)
+                    val secureFragmentDark=SecureFragmentDark()
+                    setFragment(secureFragmentDark)
+                    val cloudBasedFragmentDark=CloudBasedFragmentDark()
+                    setFragment(cloudBasedFragmentDark)
+                    ivTheme.setImageResource(R.drawable.pic_daytheme)
 
                 }
 
                 2->{
 
+                    llRoot.setBackgroundColor(R.color.white)
+                    val telegramFragment=TelegramFragment()
+                    setFragment(telegramFragment)
+                    val fastFragment=FastFragment()
+                    setFragment(fastFragment)
+                    val giftFragment=GiftFragment()
+                    setFragment(giftFragment)
+                    val powerfulFragment=PowerfulFragment()
+                    setFragment(powerfulFragment)
+                    val secureFragment=SecureFragment()
+                    setFragment(secureFragment)
+                    val cloudBasedFragment=CloudBasedFragment()
+                    setFragment(cloudBasedFragment)
                     ivTheme.setImageResource(R.drawable.pic_darktheme)
-                    llRoot.setBackgroundColor(R.drawable.bg_main)
-                    tvTitle.setTextColor(R.color.black)
-                    tvText.setTextColor(R.color.black)
                     theme=0
 
                 }
@@ -149,7 +196,13 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        
+    }
+
+    @SuppressLint("ResourceType")
+    private fun setFragment(view:Fragment){
+
+        supportFragmentManager.beginTransaction().replace(R.id.frFragment,view).commit()
 
     }
+
 }
